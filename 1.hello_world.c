fac8d20444cb4337361298c7c4cdd35d9ad1d5e8
Chan Jung <ckse93@gmail.com>
2018-12-05 03:45:13 -0600

    Create 1.hello_world.c

---
+++ 1.hello_world.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (argc, argv[]){
+  printf("hello World!\n");                       //"\n" stands for new line
+  printf("1,2,3,4,5 \n");                         //basic number printing
+  printf("%d,%d,%d,%d,%d \n", 1,2,3,4,5);         //each "%d" will print each numeric value
+  return 0;
+}
